48ef62590717f70b9a970c2250ee77c7c95c299b
VladimirForsend <vladimir@forsend.cl>
2022-04-27 20:17:48 -0400

    cambios en functions.php
    cambios en functions.php

--- chippewa_wp/functions.php
+++ chippewa_wp/functions.php
@@ -176,4 +176,4 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-include get_template_directory() . '/assets/assets.php';
+include get_template_directory() . '/assets/assets.php'; 
